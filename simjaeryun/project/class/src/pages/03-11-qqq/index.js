baac09bf562d8eb211315df70256dacbbc349da7
Simjaeryun <simjaeding@gmail.com>
2023-03-12 02:27:05 +0900

    simjaeryun 폴더 생성, react 1강 수강

---
+++ simjaeryun/project/class/src/pages/03-11-qqq/index.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled"
+
+export default function emotionPage() {
+    return <Title>안녕</Title>
+}
+
+
+const Title = styled.h1`
+font-size:30px;
+font-weight: bold;
+`
