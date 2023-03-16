5971799c5b09ca41c399083f372988cbb5be8cb0
simjaeryun <tlawofbs123@naver.com>
2023-03-16 18:47:45 +0900

    [Project][0316class] 자유게시판 페이지 Comment부분 레이아웃 완성 input text 글자크기 보여주는 기능 완성

--- simjaeryun/project/class/src/pages/index.js
+++ simjaeryun/project/class/src/pages/index.js
@@ -1,6 +1,13 @@
 import styled from "@emotion/styled"
+import { useState } from "react"
 
 export default function Home() {
+
+  const [inputComment, setInputComment] = useState("");
+  const handleCommentChange = (event) => {
+    setInputComment(event.target.value)
+  }
+
   return <Container>
     <Header>
       <h1 className="logo">
@@ -24,10 +31,27 @@ export default function Home() {
     </Navigation>
     {/* main content*/}
     <Content>
-      {/* TODO: 내일고 */}
     </Content>
-
-
+    {/* Content 관련 Button들 */}
+    <ContentButtons>
+      <button>목록으로</button>
+      <button>수정하기</button>
+      <button>삭제하기</button>
+    </ContentButtons>
+    {/* 댓글 */}
+    <Comments>
+      <h1>댓글</h1>
+      <div class="userInfo">
+        <input type="text" placeholder="작성자" />
+        <input type="password" placeholder="비밀번호" />
+      </div>
+      <div class="comment">
+        <textarea type="text" placeholder="댓글을 작성하세요" onChange={handleCommentChange} value={inputComment} maxLength={100} />
+        <div class="comment_regist">
+          <p>{inputComment.length}/100</p> <button>등록하기</button>
+        </div>
+      </div>
+    </Comments>
   </Container>
 }
 
@@ -94,8 +118,70 @@ const Content = styled.main`
 width: 1200px;
 background: #FFFFFF;
 box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
-min-height: 500px;
+min-height: 1000px;
 
 padding: 88px 105px;
+
+`
+
+const ContentButtons = styled.section`
+display:flex;
+justify-content: center;
+align-items: center;
+width:1200px;
+height:200px;
+border-bottom:3px solid #efefef;
+button{
+ width:130px;
+ height:50px;
+ background: #efefef;
+  margin-right:20px;
+}
+`
+
+const Comments = styled.section`
+width:1200px;
+margin-top:50px;
+padding-bottom:100px;
+h1{
+  margin-bottom:30px;
+  font-weight: bold;
+  font-size:20px;
+}
+.userInfo{
+  margin-bottom:20px;
+  input{
+  padding:10px;
+  border: 2px solid;
+  border-color: var(--gray5);
+  margin-right:10px;
+}
+}
+
+.comment{
+    textarea{
+    padding:10px;
+    border: 2px solid;
+    border-color: var(--gray5);
+    border-bottom:none;
+    margin-right:10px;
+    width:100%;
+    height: 200px;
+  }
+  .comment_regist{
+    display:flex;
+    justify-content: space-between;
+    width:100%;
+    height:50px;
+    border: 2px solid;
+    border-color: var(--gray5);
+    button{
+      background:#000;
+      color:#fff;
+      padding:10px;
+      cursor: pointer;
+    }
+  }
+}
 
 `
